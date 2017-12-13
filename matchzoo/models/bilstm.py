0135a690418cf06e799b0cf9a31a53386eecc3ec
FrankBlood <1062947208@qq.com>
2017-12-13 20:16:02 +0800

    my own models

---
+++ matchzoo/models/bilstm.py
@@ -0,0 +1,79 @@
+# -*- coding=utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+import keras
+import keras.backend as K
+from keras.models import Sequential, Model
+from keras.layers import *
+from keras.layers import Reshape, Embedding,Merge, Dot
+from keras.optimizers import Adam
+from model import BasicModel
+
+import sys
+sys.path.append('../matchzoo/layers/')
+sys.path.append('../matchzoo/utils/')
+from Match import *
+from utility import *
+
+class BiLSTM(BasicModel):
+    def __init__(self, config):
+        super(BiLSTM, self).__init__(config)
+        self.__name = 'BiLSTM'
+        self.check_list = [ 'text1_maxlen', 'text2_maxlen',
+                   'embed', 'embed_size', 'train_embed',  'vocab_size',
+                   'hidden_size', 'topk', 'dropout_rate']
+        self.embed_trainable = config['train_embed']
+        self.setup(config)
+        if not self.check():
+            raise TypeError('[BiLSTM] parameter check wrong')
+        print('[BiLSTM] init done', end='\n')
+
+    def setup(self, config):
+        if not isinstance(config, dict):
+            raise TypeError('parameter config should be dict:', config)
+
+        self.set_default('hidden_size', 32)
+        self.set_default('topk', 100)
+        self.set_default('dropout_rate', 0)
+        self.config.update(config)
+
+    def build(self):
+
+        query = Input(name='query', shape=(self.config['text1_maxlen'],))
+        show_layer_info('Input', query)
+        doc = Input(name='doc', shape=(self.config['text2_maxlen'],))
+        show_layer_info('Input', doc)
+
+        embedding = Embedding(self.config['vocab_size'], self.config['embed_size'], weights=[self.config['embed']], trainable = self.embed_trainable)
+        q_embed = embedding(query)
+        show_layer_info('Embedding', q_embed)
+        d_embed = embedding(doc)
+        show_layer_info('Embedding', d_embed)
+
+        rnn = Bidirectional(LSTM(units=self.config['hidden_size'], dropout=self.config['dropout_rate'],
+                                 recurrent_dropout=self.config['dropout_rate'], return_sequences=False))
+        q_rep = rnn(q_embed)
+        show_layer_info('Bidirectional-LSTM', q_rep)
+        d_rep = rnn(d_embed)
+        show_layer_info('Bidirectional-LSTM', d_rep)
+
+        merged = concatenate([q_rep, d_rep])
+        show_layer_info('Aggression of Two texts', merged)
+        merged = Dense(self.config['hidden_size'] * 2, activation='relu')(merged)
+        merged = Dropout(self.config['dropout_rate'])(merged)
+        merged = BatchNormalization()(merged)
+
+        merged = Dense(self.config['hidden_size'], activation='relu')(merged)
+        merged = Dropout(self.config['dropout_rate'])(merged)
+        merged = BatchNormalization()(merged)
+        show_layer_info('Final representation of two texts', merged)
+
+        if self.config['target_mode'] == 'classification':
+            out_ = Dense(2, activation='softmax')(merged)
+        elif self.config['target_mode'] in ['regression', 'ranking']:
+            out_ = Dense(1)(merged)
+        show_layer_info('Dense', out_)
+
+        model = Model(inputs=[query, doc], outputs=out_)
+        model.summary()
+        return model
