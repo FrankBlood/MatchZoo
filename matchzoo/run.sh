0f10812a3cb582a9678effe8b2d376407983b2b5
FrankBlood <1062947208@qq.com>
2017-12-15 22:04:33 +0800

    new all configs

--- matchzoo/run.sh
+++ matchzoo/run.sh
@@ -1,26 +1,47 @@
 # nohup python main.py --phase train --model_file models/wikiqa_config/drmm_wikiqa.config > ./logs/log.drmm.1214 &
 # python main.py --phase predict --model_file models/wikiqa_config/drmm_wikiqa.config
 
-nohup python main.py --phase train --model_file models/wikiqa_config/bimpm_wikiqa.config > ./logs/log.bimpm.1214 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/bimpm_wikiqa.config > ./logs/log.bimpm.1214 &
 # python main.py --phase predict --model_file models/wikiqa_config/bimpm_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/eattention_wikiqa.config > ./logs/log.eattention.1214 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/eattention_wikiqa.config > ./logs/log.eattention.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/eattention_wikiqa.config
 
 # nohup python main.py --phase train --model_file models/wikiqa_config/elstmattention_wikiqa.config > ./logs/log.elstmattention.1214 &
 # python main.py --phase predict --model_file models/wikiqa_config/elstmattention_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/mergedattention_wikiqa.config > ./logs/log.mergedattention.1211 &
-# python main.py --phase predict --model_file models/wikiqa_config/merged_wikiqa.config
+# nohup python main.py --phase train --model_file models/wikiqa_config/mergedattention_wikiqa.config > ./logs/log.mergedattention.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/mergedattention_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/bigru_wikiqa.config > ./logs/log.bigru.1211 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/bigru_wikiqa.config > ./logs/log.bigru.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/bigru_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/multibigru_wikiqa.config > ./logs/log.multibigru.1211 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/multibigru_wikiqa.config > ./logs/log.multibigru.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/multibigru_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/bilstm_wikiqa.config > ./logs/log.bilstm.1211 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/bilstm_wikiqa.config > ./logs/log.bilstm.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/bilstm_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/multibilstm_wikiqa.config > ./logs/log.multibilstm.1211 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/multibilstm_wikiqa.config > ./logs/log.multibilstm.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/multibilstm_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/anmm_wikiqa.config > ./logs/log.anmm.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config_anmm_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/arci_wikiqa.config > ./logs/log.arci.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/arci_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/arcii_wikiqa.config > ./logs/log.arcii.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/arcii_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/cdssm_wikiqa.config > ./logs/log.cdssm.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/cdssm_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/dssm_wikiqa.config > ./logs/log.dssm.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/dssm_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/duet_wikiqa.config > ./logs/log.duet.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/duet_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/mvlstm_wikiqa.config > ./logs/log.mvlstm.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/mvlstm_wikiqa.config
