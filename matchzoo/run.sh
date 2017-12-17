2244e8474afcbb7019789dfa41e0b1f498179181
FrankBlood <1062947208@qq.com>
2017-12-17 10:42:24 +0800

    cautious

--- matchzoo/run.sh
+++ matchzoo/run.sh
@@ -4,7 +4,7 @@
 # nohup python main.py --phase train --model_file models/wikiqa_config/bimpm_wikiqa.config > ./logs/log.bimpm.1214 &
 # python main.py --phase predict --model_file models/wikiqa_config/bimpm_wikiqa.config
 
-# nohup python main.py --phase train --model_file models/wikiqa_config/eattention_wikiqa.config > ./logs/log.eattention.1215 &
+# nohup python main.py --phase train --model_file models/wikiqa_config/eattention_wikiqa.config > ./logs/log.eattention.1216 &
 # python main.py --phase predict --model_file models/wikiqa_config/eattention_wikiqa.config
 
 # nohup python main.py --phase train --model_file models/wikiqa_config/elstmattention_wikiqa.config > ./logs/log.elstmattention.1214 &
@@ -26,7 +26,7 @@
 # python main.py --phase predict --model_file models/wikiqa_config/multibilstm_wikiqa.config
 
 # nohup python main.py --phase train --model_file models/wikiqa_config/anmm_wikiqa.config > ./logs/log.anmm.1215 &
-# python main.py --phase predict --model_file models/wikiqa_config_anmm_wikiqa.config
+# python main.py --phase predict --model_file models/wikiqa_config/anmm_wikiqa.config
 
 # nohup python main.py --phase train --model_file models/wikiqa_config/arci_wikiqa.config > ./logs/log.arci.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/arci_wikiqa.config
@@ -45,3 +45,33 @@
 
 # nohup python main.py --phase train --model_file models/wikiqa_config/mvlstm_wikiqa.config > ./logs/log.mvlstm.1215 &
 # python main.py --phase predict --model_file models/wikiqa_config/mvlstm_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/matchpyramid_wikiqa.config > ./logs/log.matchpyramid.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/matchpyramid_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/drmm_tks_wikiqa.config > ./logs/log.drmm_tks.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/drmm_tks_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/eselfattention_wikiqa.config > ./logs/log.eselfattention.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/eselfattention_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecrossattention_wikiqa.config > ./logs/log.ecrossattention.1215 &
+# python main.py --phase predict --model_file models/wikiqa_config/ecrossattention_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecautiousattention_wikiqa.config > ./logs/log.ecautiousattention.1216 &
+# python main.py --phase predict --model_file models/wikiqa_config/ecautiousattention_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecautiousattention1_wikiqa.config > ./logs/log.ecautiousattention1.1216 &
+# python main.py --phase predict --model_file models/wikiqa_config/ecautiousattention1_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecautiousattention2_wikiqa.config > ./logs/log.ecautiousattention2.1216 &
+# python main.py --phase predict --model_file models/wikiqa_config/ecautiousattention2_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecautiousattention3_wikiqa.config > ./logs/log.ecautiousattention3.1216 &
+# python main.py --phase predict --model_file models/wikiqa_config/ecautiousattention3_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecautiousattention4_wikiqa.config > ./logs/log.ecautiousattention4.1216 &
+# python main.py --phase predict --model_file models/wikiqa_config/ecautiousattention4_wikiqa.config
+
+# nohup python main.py --phase train --model_file models/wikiqa_config/ecautiousattentionall_wikiqa.config > ./logs/log.ecautiousattentionall.1216 &
+python main.py --phase predict --model_file models/wikiqa_config/ecautiousattentionall_wikiqa.config
